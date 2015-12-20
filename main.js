b43dae48a6ff9c0139e275a6d2c53a543742d593
timwis <tim@timwis.com>
2015-12-19 19:08:35 -0500

    standard style

--- main.js
+++ main.js
@@ -33,72 +33,72 @@ var fieldObjectFields = {
   dataset: 'field_21'
 }
 
-function generateOptions(val) {
+function generateOptions (val) {
   var options = []
   var mappedType = typeMap[val] || ''
-  
-  fieldTypes.forEach(function(fieldType) {
+
+  fieldTypes.forEach(function (fieldType) {
     var selected = fieldType === mappedType ? ' selected="selected"' : ''
     options.push('<option value="' + fieldType + '"' + selected + '>' + fieldType + '</option>')
   })
   return options.join('')
 }
 
-function generateFieldTable(fields) {
+function generateFieldTable (fields) {
   var markup = [
     '<form id="fieldsForm">',
-      '<table class="table">',
-        '<thead>',
-          '<tr>',
-            '<th>Name</th>',
-            '<th>Alias</th>',
-            '<th>Type</th>',
-            '<th>Description</th>',
-          '</tr>',
-        '</thead>',
-        '<tbody>'
+    '<table class="table">',
+    '<thead>',
+    '<tr>',
+    '<th>Name</th>',
+    '<th>Alias</th>',
+    '<th>Type</th>',
+    '<th>Description</th>',
+    '</tr>',
+    '</thead>',
+    '<tbody>'
   ]
-  
-  fields.forEach(function(field) {
+
+  fields.forEach(function (field) {
     markup = markup.concat([
-        '<tr>',
-          '<td><input type="text" name="name" value="' + field.name + '"></td>',
-          '<td><input type="text" name="alias" value="' + field.aliasName + '"></td>',
-          '<td>',
-      		'<select name="type">',
-      		  generateOptions(field.type),
-      		'</select>',
-      	  '</td>',
-          '<td><input type="text" name="description"></td>',
-        '</tr>'
+      '<tr>',
+      '<td><input type="text" name="name" value="' + field.name + '"></td>',
+      '<td><input type="text" name="alias" value="' + field.aliasName + '"></td>',
+      '<td>',
+      '<select name="type">',
+      generateOptions(field.type),
+      '</select>',
+      '</td>',
+      '<td><input type="text" name="description"></td>',
+      '</tr>'
     ])
   })
-  
+
   markup = markup.concat([
-        '</tbody>',
-      '</table>',
-      '<input type="submit">',
+    '</tbody>',
+    '</table>',
+    '<input type="submit">',
     '</form>'
   ])
-                          
+
   return markup.join('')
 }
 
-function submitFields(fields) {
+function submitFields (fields) {
   var pending = 0
   Knack.showSpinner()
-  
-  function checkIfDone() {
-    if(pending < 1) {
+
+  function checkIfDone () {
+    if (pending < 1) {
       Knack.hideSpinner()
     }
   }
-  
-  fields.forEach(function(field) {
+
+  fields.forEach(function (field) {
     pending++
-    
+
     field[fieldObjectFields.dataset] = Knack.hash_id
-    
+
     var url = 'https://api.knackhq.com/v1/scenes/scene_125/views/view_246/records/'
     $.ajax({
       url: url,
@@ -110,7 +110,7 @@ function submitFields(fields) {
         'X-Knack-Application-Id': '565b5b0f8e115a4c760607e8',
         'X-Knack-REST-API-Key': 'knack'
       },
-      complete: function() {
+      complete: function () {
         pending--
         checkIfDone()
       }
@@ -118,45 +118,44 @@ function submitFields(fields) {
   })
 }
 
-$(document).on('knack-scene-render.scene_124', function(event, view, data) {
+$(document).on('knack-scene-render.scene_124', function (event, view, data) {
   $('#view_245').empty().append(searchTemplate)
-  
+
   // Listen to the submission of the search form
-  $('#layerForm').on('submit', function(e) {
+  $('#layerForm').on('submit', function (e) {
     var layerName = e.currentTarget.layerName.value
     Knack.showSpinner()
-    
+
     // Query the sde-metadata-api with the search input
     $.ajax({
       url: 'https://api.phila.gov/sde-metadata-api/v1/feature-classes/' + layerName,
       dataType: 'json',
-      success: function(data) {
-      	Knack.hideSpinner()
+      success: function (data) {
+        Knack.hideSpinner()
         $('#layerFormStatus').css('display', 'none')
         var markup = generateFieldTable(data.fields)
         $('#view_247').empty().append(markup)
       },
-      error: function(xhr, status, msg) {
+      error: function (xhr, status, msg) {
         $('#layerFormStatus').html('<p>Error: ' + msg + '</p>').css('display', 'inline-block')
-       	Knack.hideSpinner()
+        Knack.hideSpinner()
       }
     })
-    
+
     e.preventDefault()
   })
 
   // Listen to the submission of the fields table form
-  $('#view_247').on('submit', '#fieldsForm', function(e) {
+  $('#view_247').on('submit', '#fieldsForm', function (e) {
     // Cleverly construct an array of field objects from the form data
     var fields = []
-    ;['name', 'alias', 'type', 'description'].forEach(function(attr) {
-      ;[].forEach.call(e.currentTarget[attr], function(element, index) {
-        if( ! fields[index]) fields[index] = {}
-        var fieldObjectField = fieldObjectFields[attr]
-        fields[index][fieldObjectField] = element.value
+      ;['name', 'alias', 'type', 'description'].forEach(function (attr) {
+        ;[].forEach.call(e.currentTarget[attr], function (element, index) {
+          if (!fields[index]) fields[index] = {}
+          var fieldObjectField = fieldObjectFields[attr]
+          fields[index][fieldObjectField] = element.value
+        })
       })
-    })
-    console.log('submitted', fields)
     submitFields(fields)
     e.preventDefault()
   })
